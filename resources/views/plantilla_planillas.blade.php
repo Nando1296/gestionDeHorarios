163c3457ea3d3d04e24ab81650a0391dd52b993c
DafneXCA <cruzdafne123@gmail.com>
2023-11-07 14:02:24 -0400

    env

--- resources/views/plantilla_planillas.blade.php
+++ resources/views/plantilla_planillas.blade.php
@@ -26,7 +26,8 @@
   <header>
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><span id="Nlogo">Aulapp</span><img src="{{asset('Imagenes/logo.jpeg')}}" width="50" id="logo"></a>
+        <img src="{{asset('imagenes/INF-SIS.png')}}" alt="" >
+        <div class="titulo">Departamento de Informática y Sistemas </div>
         @yield('Titulo')
         <a href="#" class="material-symbols-outlined" id="menu">menu</a>
         <form class="d-flex">
